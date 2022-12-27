edcfc96a4f540df601792202d9c40fd28469b220
prathiba <114104880+prathibavg@users.noreply.github.com>
2022-12-27 12:40:37 +0530

    Add files via upload

---
+++ SumEven.java
@@ -0,0 +1,21 @@
+public class SumEven {
+    public static void main(String[] args) {
+        // int a=1;
+        // int sum=0;
+        // while(a<=10){
+        //     if(a%2==0){
+        //         sum=sum+a;
+        //     }
+        //     a++;
+        //     // System.out.println(sum);
+        // }
+        int sum=0;
+        for(int i=0;i<=10;i++){
+            if(i%2==0){   
+                sum=sum+i;
+                  }
+        }
+        System.out.println(sum);
+
+    }
+}
